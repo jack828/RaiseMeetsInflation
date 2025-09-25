55236e59d7ec709b1f4e6bd5f32132a2d4abec21
Jack Burgess <jackburgess124@gmail.com>
2025-09-25 12:28:56 +0100

    Link ko-fi in advert slot

--- src/components/advert.tsx
+++ src/components/advert.tsx
@@ -1,10 +1,22 @@
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
+
 export const Advert = () => {
   return (
     <div className="flex flex-col rounded-large transition-transform-background border-dashed border-3 p-10 text-center text-current">
       <p className="">Advert Placeholder</p>
       <p className="text-sm">gotta pay the bills somehow</p>
       <p className="text-sm">Don&apos;t like ads? Me neither.</p>
-      <p className="text-sm">Consider supporting this site on ko-fi TODO external link icon</p>
+      <p className="text-sm">
+        <a
+          href="https://ko-fi.com/jack828"
+          target="_blank"
+          rel="noopener"
+          className="text-blue-500 hover:text-blue-600 hover:underline"
+        >
+          Consider supporting me on Ko-fi instead{' '}
+          <ArrowTopRightOnSquareIcon className="size-4 inline" />
+        </a>
+      </p>
     </div>
   )
 }
